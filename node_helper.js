6a9cd8664d4fdd07950cf79f6b620085478a69e2
Cassioblu55 <cassioblubyrd@gmail.com>
2020-07-27 03:41:42 -0500

    Updated Kaija time tracker

--- node_helper.js
+++ node_helper.js
@@ -9,7 +9,7 @@ var numeral = require('numeral');
 module.exports = NodeHelper.create({
 
     getMinutesLeft() {
-        let diff = Math.floor(new DateDiff(new Date(2021, 10, 5), new Date()).minutes());
+        let diff = Math.floor(new DateDiff(new Date(2020, 8, 24), new Date()).minutes());
         var formatted = numeral(diff).format('0,0');
         return formatted;
     },
